8078ff53263ee9e931599d6f857b4c5e766b161f
Iferes <49124852+Iferes@users.noreply.github.com>
2019-04-12 01:12:17 +0200

    Update settings.py

--- webscraping/settings.py
+++ webscraping/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'webscraping.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'webscraping (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
 
 # Obey robots.txt rules
 #ROBOTSTXT_OBEY = True
@@ -65,9 +66,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'webscraping.pipelines.WebscrapingPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'webscraping.pipelines.WebscrapingPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
